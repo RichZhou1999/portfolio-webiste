b8e3b9944033c9fa0da6d583f524d43b1679b177
RichZhou1999 <373072096@qq.com>
2023-01-05 17:10:51 -0800

    first commit

---
+++ src/Components/About/About.js
@@ -0,0 +1,59 @@
+import AnimatedLetters from "../AnimatedLetters/AnimatedLetters";
+import "./About.scss"
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCss3, faGitAlt, faJava, faJsSquare, faPython, faReact } from "@fortawesome/free-brands-svg-icons";
+
+const About = ()=>{
+    const [letterClass, setLetterClass] = useState('text-animate')
+    return (
+        <>
+        <div className="about-page">
+            <div className="text-zone">
+                <h1>
+                    <AnimatedLetters
+                    letterClass={letterClass}
+                    strArray={['A',"b",'o','u','t'," ","m","e"]}
+                    idx={15}
+                    />
+                </h1> 
+                <p>
+                My name is RRRRRRRRRRRRRRRRRRRRRRRRRRRR
+                </p>
+                <p>
+                My name is RRRRRRRRRRRRRRRRRRRRRRRRRRRR
+                </p>
+                <p>
+                My name is RRRRRRRRRRRRRRRRRRRRRRRRRRRR
+                </p>
+
+            </div>
+            <div className="stage-cube-cont">
+                <div className='cubespinner'>
+                    <div className="face1">
+                        <FontAwesomeIcon icon={faReact} color="#5ED4F4"/>
+                    </div>
+                    <div className="face2">
+                        <FontAwesomeIcon icon={faCss3} color="#28A4D9"/>
+                    </div>
+                    <div className="face3">
+                        <FontAwesomeIcon icon={faPython} color="#5ED4F4"/>
+                    </div>
+                    <div className="face4">
+                        <FontAwesomeIcon icon={faJava} color="brown"/>
+                    </div>
+                    <div className="face5">
+                        <FontAwesomeIcon icon={faGitAlt} color="#EC4D2A"/>
+                    </div>
+                    <div className="face6">
+                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D"/>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+        </>
+    )
+}
+export default About;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import LogoTitle from "../../assets/images/logo-s.png"
+import AnimatedLetters from "../AnimatedLetters/AnimatedLetters";
+import "./Home.scss"
+
+const Home = ()=>{
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ["Y","i","h","u","a","", "Z","h","o","u"]
+    const schoolArray = ["U","C","","B","e","r","k","l","e","y"]
+    const majorArray = ["S","y","s","t","e","m","","E","n","g","i","n","e","e","r","i","n","g"]
+    useEffect (()=>{
+        setTimeout(()=>{
+            setLetterClass("text-animate-hover")
+        },4000)
+    },[])
+    return (
+        <>
+        <div>123</div>
+        <div className="home-page">
+        <div className="text-zone">
+            <h1>Hi, <br/> I'm
+            <AnimatedLetters letterClass={letterClass} 
+            strArray={nameArray}
+            idx={15}/>
+             <br/>
+             <AnimatedLetters letterClass={letterClass} 
+            strArray={schoolArray}
+            idx={22}/>
+             <br/>
+             </h1>
+             <h2>System Engineering</h2>
+             <Link to="/contact" className="flat-button">CONTACT ME</Link>
+        </div>
+        </div>
+        </>
+    )
+}
+export default Home;
